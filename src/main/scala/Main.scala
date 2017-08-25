d83c1ac388aba124147fc470982eaf2e195b1bae
clarkdm <dmclark@hotmail.co.uk>
2017-08-25 15:29:42 +0100

    25/8/17

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -11,7 +11,7 @@ import scala.util.{Failure, Success}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
-object Main extends App{
+object Main extends App {
   // The config string refers to mysqlDB that we defined in application.conf
   val db = Database.forConfig("mysqlDB")
 
@@ -25,14 +25,25 @@ object Main extends App{
   // schema definition to generate a CREATE TABLE command
   val initPeopleCmd = DBIO.seq(peopleTable.schema.create)
 
+
+  println("\n\n")
   dropDB
-//  initialisePeople
+
+
+  //Search_people
+  //update_people(23)
+
+  //  initialisePeople
   def dropDB = {
+    println("\n dropDB \n")
+
     //do a drop followed by initialisePeople
-    val dropFuture = Future{db.run(dropPeopleCmd)}
+    val dropFuture = Future {
+      db.run(dropPeopleCmd)
+    }
     //Attempt to drop the table, Await does not block here
-    Await.result(dropFuture, Duration.Inf).andThen{
-      case Success(_) =>  initialisePeople
+    Await.result(dropFuture, Duration.Inf).andThen {
+      case Success(_) => initialisePeople
       case Failure(error) =>
         println("Dropping the table failed due to: " + error.getMessage)
         initialisePeople
@@ -41,12 +52,14 @@ object Main extends App{
 
 
   def initialisePeople = {
+    println("\n initialisePeople \n")
+
     //initialise people
-    val setupFuture =  Future {
+    val setupFuture = Future {
       db.run(initPeopleCmd)
     }
     //once our DB has finished initializing we are ready to roll, Await does not block
-    Await.result(setupFuture, Duration.Inf).andThen{
+    Await.result(setupFuture, Duration.Inf).andThen {
       case Success(_) => runQuery
       case Failure(error) =>
         println("Initialising the table failed due to: " + error.getMessage)
@@ -54,6 +67,8 @@ object Main extends App{
   }
 
   def runQuery = {
+    println("\n runQuery \n")
+
     val insertPeople = Future {
       val query = peopleTable ++= Seq(
         (10, "Jack", "Wood", 36),
@@ -70,39 +85,167 @@ object Main extends App{
       db.run(query)
     }
     Await.result(insertPeople, Duration.Inf).andThen {
-      case Success(_) => listPeople
+      case Success(result) => println(result); listPeople
       case Failure(error) => println("Welp! Something went wrong! " + error.getMessage)
     }
   }
 
 
-
   def listPeople = {
+    println("\n listPeople \n")
+
     val queryFuture = Future {
-      // simple query that selects everything from People and prints them out
-      db.run(peopleTable.result).map(_.foreach {
-        case (id, fName, lName, age) => println(s" $id $fName $lName $age")
-      })
+
+        val q = peopleTable
+        val updateAction = q.result
+        val sql = updateAction.statements.head
+        println(sql)
+        db.run(updateAction)
+
     }
+
     Await.result(queryFuture, Duration.Inf).andThen {
-      case Success(_) =>  Count_people
+      case Success(result) => println(result); update_people(234)
       case Failure(error) =>
         println("Listing people failed due to: " + error.getMessage)
     }
   }
-  def Count_people = {
+
+  def update_people(x: Int) = {
+    println("\n update_people \n")
+
     val queryFuture = Future {
-      // simple query that selects everything from People and prints them out
-      db.run(peopleTable.result).map(_.foreach {
-        case (id, fName, lName, age) => println(s" $id $fName $lName $age")
-      })
+      val q = for {c <- peopleTable if c.fName === "jef"} yield c.age
+      val updateAction = q.update(x)
+      val sql = q.updateStatement
+      println(sql)
+      db.run(updateAction)
     }
     Await.result(queryFuture, Duration.Inf).andThen {
-      case Success(_) =>  db.close()  //cleanup DB connection
+      case Success(result) => println(result); delete_people //cleanup DB connection
+      case Failure(error) =>
+        println("Listing people failed due to: " + error.getMessage)
+    }
+
+  }
+
+  def delete_people = {
+    println("\n delete_people \n")
+
+    val queryFuture = Future {
+      val q = for {c <- peopleTable if c.fName === "bob"} yield c
+      val updateAction = q.delete
+
+      val sql = updateAction.statements.head
+      println(sql)
+      db.run(updateAction)
+    }
+    Await.result(queryFuture, Duration.Inf).andThen {
+      case Success(result) => println(result); Search_people //cleanup DB connection
       case Failure(error) =>
         println("Listing people failed due to: " + error.getMessage)
     }
   }
+
+  def Search_people = {
+    println("\n Search_people \n")
+
+    val queryFuture = Future {
+
+
+      val q = for {c <- peopleTable if c.fName === "Jack"} yield c
+      val updateAction = q.result
+      val sql = updateAction.statements.head
+
+      println(sql)
+      db.run(updateAction)
+    }
+    Await.result(queryFuture, Duration.Inf).andThen {
+      case Success(result) => println(result); num_of_people //cleanup DB connection
+      case Failure(error) =>
+        println("Listing people failed due to: " + error.getMessage)
+    }
+  }
+
+  def num_of_people = {
+    println("\n num_of_people \n")
+    val queryFuture = Future {
+      val q = peopleTable.length
+      val updateAction = q.result
+      val sql = updateAction.statements.head
+
+      println(sql)
+      db.run(updateAction)
+    }
+    Await.result(queryFuture, Duration.Inf).andThen {
+      case Success(result) => println(result); average_age_people //cleanup DB connection
+      case Failure(error) =>
+        println("Listing people failed due to: " + error.getMessage)
+    }
+  }
+  def average_age_people = {
+    println("\n num_of_people \n")
+    val queryFuture = Future {
+      val q = peopleTable.map(_.age).avg
+      val updateAction = q.result
+      val sql = updateAction.statements.head
+
+      println(sql)
+      db.run(updateAction)
+    }
+    Await.result(queryFuture, Duration.Inf).andThen {
+      case Success(result) => println(result); most_common_fName_people //cleanup DB connection
+      case Failure(error) =>
+        println("Listing people failed due to: " + error.getMessage)
+    }
+  }
+
+
+  def most_common_fName_people = {
+    println("\n most_common_fName_people \n")
+    val queryFuture = Future {
+      val q = peopleTable.groupBy { _.fName }
+        .map { case (name, group) =>
+          (name , group.length)
+        }.sortBy(_._2.desc)
+
+
+      val updateAction = q.result.head
+      val sql = updateAction.statements.head
+
+      println(sql)
+      db.run(updateAction)
+    }
+    Await.result(queryFuture, Duration.Inf).andThen {
+      case Success(result) => println(result); most_common_lName_people //cleanup DB connection
+      case Failure(error) =>
+        println("Listing people failed due to: " + error.getMessage)
+    }
+  }
+
+  def most_common_lName_people = {
+    println("\n most_common_lName_people \n")
+    val queryFuture = Future {
+      val q = peopleTable.groupBy { _.lName }
+        .map { case (name, group) =>
+          (name , group.length)
+        }.sortBy(_._2.desc)
+
+
+      val updateAction = q.result//.head
+      val sql = updateAction.statements.head
+
+      println(sql)
+      db.run(updateAction)
+    }
+    Await.result(queryFuture, Duration.Inf).andThen {
+      case Success(result) => println(result); db.close() //cleanup DB connection
+      case Failure(error) =>
+        println("Listing people failed due to: " + error.getMessage)
+    }
+  }
+
+
 
 
 }
